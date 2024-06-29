0a10d96960dcc26a3c3d3d64a9ecadbcef86596f
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-06-29 11:59:24 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,7 @@
-/* eslint-disable */
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Response from API');
+    }, 1000);
+  });
 }
