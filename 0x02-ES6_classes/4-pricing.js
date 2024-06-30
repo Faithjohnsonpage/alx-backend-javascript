95fe5f3ac0733f95a0069b13da3060c5f23f7a49
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-06-30 13:26:38 +0100

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,27 +4,27 @@ export default class Pricing {
     this._currency = currency;
   }
 
+  set amount(amount) {
+    this._amount = amount;
+  }
+
   get amount() {
     return this._amount;
   }
 
-  set amount(a) {
-    this._amount = a;
+  set currency(currency) {
+    this._currency = currency;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(c) {
-    this._currency = c;
-  }
-
   displayFullPrice() {
-    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    return (amount * conversionRate);
+    return amount * conversionRate;
   }
 }
