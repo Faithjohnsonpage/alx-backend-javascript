6f6ffa7ce1e9787a1ed4cb77b7879e0e0dc6393c
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-07-02 03:22:40 +0100

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,5 @@ export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  const studentIds = studentList.map((element) => element.id);
-  return studentIds;
+  return studentList.map((element) => element.id);
 }
