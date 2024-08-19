04f0f20d0bfc189c4bfb6f8a11a57b702b288b4c
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-08-19 18:37:42 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
   process.on('exit', () => {
-    console.log('This important software is now closing\n');
+    process.stdout.write('This important software is now closing\n');
   });
 }
